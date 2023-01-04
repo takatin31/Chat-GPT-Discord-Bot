02649028b56c510ab2d3cced75096a20eb726d23
Ali Cherif <ga_hammas@esi.dz>
2023-01-04 14:16:41 +0100

    Initial commit

---
+++ exceptions/__init__.py
@@ -0,0 +1,29 @@
+""""
+Copyright © Krypton 2019-2022 - https://github.com/kkrypt0nn (https://krypton.ninja)
+Description:
+🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
+
+Version: 5.4.1
+"""
+
+from discord.ext import commands
+
+
+class UserBlacklisted(commands.CheckFailure):
+    """
+    Thrown when a user is attempting something, but is blacklisted.
+    """
+
+    def __init__(self, message="User is blacklisted!"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UserNotOwner(commands.CheckFailure):
+    """
+    Thrown when a user is attempting something, but is not an owner of the bot.
+    """
+
+    def __init__(self, message="User is not an owner of the bot!"):
+        self.message = message
+        super().__init__(self.message)
